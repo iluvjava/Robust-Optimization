2eba98f93960e4367a593b3ab54d93320c1c0080
Alto Legato <victor1301166040@gmail.com>
2022-03-30 12:41:07 -0700

    >w<

---
+++ examples/knapsack.jl
@@ -0,0 +1,31 @@
+using JuMP
+import HiGHS, GLPK
+import Test
+
+function example_knapsack(; verbose = true)
+    profit = [5, 3, 2, 7, 4]
+    weight = [2, 8, 4, 2, 5]
+    capacity = 10
+    model = Model(GLPK.Optimizer)
+    @variable(model, x[1:5], Bin)
+    # Objective: maximize profit
+    @objective(model, Max, profit' * x)
+    # Constraint: can carry all
+    @constraint(model, weight' * x <= capacity)
+    # Solve problem using MIP solver
+    optimize!(model)
+    if verbose
+        println("Objective is: ", objective_value(model))
+        println("Solution is:")
+        for i in 1:5
+            print("x[$i] = ", value(x[i]))
+            println(", p[$i]/w[$i] = ", profit[i] / weight[i])
+        end
+    end
+    Test.@test termination_status(model) == OPTIMAL
+    Test.@test primal_status(model) == FEASIBLE_POINT
+    Test.@test objective_value(model) == 16.0
+    return
+end
+
+example_knapsack()
