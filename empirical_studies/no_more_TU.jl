12ef549e817568f14967310c78e5817913ee260f
Alto Legato <victor1301166040@gmail.com>
2022-05-25 20:49:53 -0700

    >w<

--- empirical_studies/no_more_TU.jl
+++ empirical_studies/no_more_TU.jl
@@ -12,21 +12,20 @@ function MakeModel(B::Int=6, J::Int=100; ip::Bool=false)
         ρ⁺ = @variable(model, ρ⁺[1:B], lower_bound=0, upper_bound=1)
         ρ⁻ = @variable(model, ρ⁻[1:B], lower_bound=0, upper_bound=1)
     end
-    ξ⁺ = @variable(model, ξ⁺[1:B])
-    ξ⁻ = @variable(model, ξ⁻[1:B])
+    ξ⁺ = @variable(model, ξ⁺[1:B, 1:J])
+    ξ⁻ = @variable(model, ξ⁻[1:B, 1:J])
     λ = @variable(model, λ[1:J], lower_bound=-1, upper_bound=0)
 
-    @constraint(model,[b=1:B, j=1:J], -ρ⁺[b] <= ξ⁺[b])
-    @constraint(model,[b=1:B, j=1:J], ξ⁺[b] <= ρ⁺[b])
-    @constraint(model,[b=1:B, j=1:J], λ[j] - (1 - ρ⁺[b])<=ξ⁺[b])
-    @constraint(model,[b=1:B, j=1:J], ξ⁺[b]<=λ[j] + (1 - ρ⁺[b]))
+    @constraint(model,[b=1:B, j=1:J], -ρ⁺[b] <= ξ⁺[b, j])
+    @constraint(model,[b=1:B, j=1:J], ξ⁺[b, j] <= ρ⁺[b])
+    @constraint(model,[b=1:B, j=1:J], λ[j] - (1 - ρ⁺[b])<=ξ⁺[b, j])
+    @constraint(model,[b=1:B, j=1:J], ξ⁺[b, j]<=λ[j] + (1 - ρ⁺[b]))
 
-    @constraint(model,[b=1:B, j=1:J], -ρ⁻[b] <= ξ⁻[b])
-    @constraint(model,[b=1:B, j=1:J], ξ⁻[b] <= ρ⁻[b])
-    @constraint(model,[b=1:B, j=1:J], λ[j] - (1 - ρ⁻[b]) <= ξ⁻[b])
-    @constraint(model,[b=1:B, j=1:J], ξ⁻[b] <= λ[j] + (1 - ρ⁻[b]))
+    @constraint(model,[b=1:B, j=1:J], -ρ⁻[b] <= ξ⁻[b, j])
+    @constraint(model,[b=1:B, j=1:J], ξ⁻[b, j] <= ρ⁻[b])
+    @constraint(model,[b=1:B, j=1:J], λ[j] - (1 - ρ⁻[b]) <= ξ⁻[b, j])
+    @constraint(model,[b=1:B, j=1:J], ξ⁻[b, j] <= λ[j] + (1 - ρ⁻[b]))
     @constraint(model,[b=1:B], ρ⁺[b] + ρ⁻[b] == 1)
-
 
 return model, vcat(ξ⁺[:], ξ⁻[:], λ[:]) end
 
@@ -45,21 +44,11 @@ function RunThis()
     λ = model[:λ]
     ξ⁺ = model[:ξ⁺]
     ξ⁻ = model[:ξ⁻]
-    @objective(model, Max, dot(λ, H*d) + γ*sum(H*ξ⁺) - γ*sum(H*ξ⁻))
+    @objective(model, Max, dot(λ, H*d) + γ*dot(H'[:], ξ⁺[:]) - γ*dot(H'[:], ξ⁻[:]))
     optimize!(model)
     ObjectVal = objective_value(model)
-    
-    @info "MIP Model Solve: "
-    model_mip, vars = MakeModel(B, J, ip=true)
-    λ = model_mip[:λ]
-    ξ⁺ = model_mip[:ξ⁺]
-    ξ⁻ = model_mip[:ξ⁻]
-    @objective(model_mip, Max, dot(λ, H*d) + γ*sum(H*ξ⁺) - γ*sum(H*ξ⁻))
-    optimize!(model_mip)
-    ObjectVal_mip = objective_value(model_mip)
-
     println("Obj Val LP: $(ObjectVal)")
-    println("Obj Val MIP: $(ObjectVal_mip)")
-return model, model_mip end
+    
+return model end
 
-model, model_mip = RunThis()
+model = RunThis()
--- src/problem_parameters.jl
+++ src/problem_parameters.jl
@@ -22,7 +22,7 @@ mutable struct ConstParameters
 
     function ConstParameters()
         this = new()
-        this.HORIZON = 6; 
+        this.HORIZON = 4; 
         this.Φ = 1000000; 
         this.RREGD = zeros(this.HORIZON)
         this.RREGU = zeros(this.HORIZON)
