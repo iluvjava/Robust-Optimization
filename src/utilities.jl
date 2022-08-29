dfe041f18055f9b707344403a68472565370cccc
Alto Legato <victor1301166040@gmail.com>
2022-08-29 12:01:00 -0700

    >c<

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,2 +1,8 @@
-ENV["GUROBI_HOME"] = "/Library/gurobi950/mac64"
+@static if Sys.islinux()
+    @error "I don't know what to do if it's linux"
+elseif Sys.isapple()
+    ENV["GUROBI_HOME"] = "/Library/gurobi950/mac64"
+elseif Sys.iswindows()
+    ENV["GUROBI_HOME"] = "C:\\gurobi952\\win64"
+end
 using SparseArrays, LinearAlgebra, JuMP, GLPK, HiGHS, Gurobi, Plots
