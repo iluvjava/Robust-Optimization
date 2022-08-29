3a79aefb0e60a646216ea3d94f992036cf1f0dc9
AL <32211153+iluvjava@users.noreply.github.com>
2022-08-29 11:46:38 -0700

    >w<

--- tests/ccga_loops.jl
+++ tests/ccga_loops.jl
@@ -8,7 +8,6 @@ include("../src/ccga_modeling.jl")
 const GUROBI_ENV = Gurobi.Env()
 
 
-
 """
     A struct that contains all the parameters involved for the CCGA inner forloop iterations. 
 """
@@ -37,9 +36,10 @@ end
 """
 mutable struct CCGAResults
     inner_loops::Vector{CCGAInnerloopParameters}
+    msp::Union{MSP, Nothing}
     
     function CCGAResults()
-    return new(Vector{CCGAInnerloopParameters}()) end
+    return new(Vector{CCGAInnerloopParameters}(), nothing) end
 end
 
 """
@@ -74,8 +74,9 @@ function CCGAInnerLoop(
     upperbound_list = Vector{Float64}()
     all_qs = Vector{Vector}()
     all_ds = Vector{Vector}()
-    model_fmp = Model(with_optimizer(Gurobi.Optimizer, GUROBI_ENV)); set_silent(model_fmp)
+    model_fmp = Model(() -> Gurobi.Optimizer(GUROBI_ENV)); set_silent(model_fmp)
     fmp = FMP(w̄, γ̄, d̂, model_fmp, sparse_vee=sparse_vee)
+    @info "Inner loop is initialized with fmp, and we are solving the initial fmp. "
     Solve!(fmp)
     if objective_value(fmp)|>isnan
         DebugReport(fmp, "fmp_debug_report_inner_ccga")
@@ -85,12 +86,12 @@ function CCGAInnerLoop(
     push!(upperbound_list, objective_value(fmp))
     
     fsp = nothing
-    for _ in 1:max_iter
+    for II in 1:max_iter
         d = GetDemandVertex(fmp); push!(all_ds, d)
-        model_fsp = Model(with_optimizer(Gurobi.Optimizer, GUROBI_ENV)); set_silent(model_fsp)
+        model_fsp = Model(() -> Gurobi.Optimizer(GUROBI_ENV)); set_silent(model_fsp)
         fsp = FSP(w̄, d, model_fsp, sparse_vee=sparse_vee)
         Solve!(fsp); push!(lowerbound_list, fsp |> objective_value)
-        @info "(FSP Lower, FMP Upper) = $((lowerbound_list[end], upperbound_list[end]))"
+        @info "(FSP Lower, FMP Upper) = $((lowerbound_list[end], upperbound_list[end])) at itr = $II"
         if lowerbound_list[end] > ϵ
             @info "Inner CCGA forloop terminated due to a positive lower of bound of"*
             ": $(lowerbound_list[end]) from FSP which is higher than: ϵ=$(ϵ)."
@@ -98,6 +99,7 @@ function CCGAInnerLoop(
         end
         q = Getq(fsp); push!(all_qs, q)
         Introduce!(fmp, q)
+        @info "CCGA Inner loop continues, constraints is introduced to fmp and we are solving it. "
         Solve!(fmp);push!(upperbound_list, objective_value(fmp))
         @assert !(objective_value(fmp)|>isnan) "$(premise) FMP"*
             " is infeasible or unbounded DURING the inner CCGA iterations. "
@@ -135,23 +137,23 @@ return CCGAInnerloopParameters(
 """
 function CCGAOutterLoop(
     d_hat::Vector{N1}, 
-    gamma_upper::N2, 
+    gamma_upper::N2;
     epsilon::N3=0.1, 
     inner_max_itr::Int=10,
-    outter_max_itr::Int=10,
-    make_plot::bool=true
+    outter_max_itr::Int=5,
+    make_plot::Bool=true
 ) where {N1 <: Number, N2 <: Number, N3 <: Number}
 
     context = "During the execution of the outter loop of CCGA: "
     @assert length(d_hat) == size(MatrixConstruct.H, 2) "$(context)"*
     "The length of d_hat is wrong, it's $(length(d_hat)), but we want: $(size(MatrixConstruct.H, 2)). "
     @assert gamma_upper > 0 "$context gamma_upper should be a strictly positive number."
-    @assert epsilong > 0 "$context parameter epsilon should be strictly positive but got: $epsilon. "
+    @assert epsilon > 0 "$context parameter epsilon should be strictly positive but got: $epsilon. "
     @assert inner_max_itr > 0 && outter_max_itr >0 "$context both the inner_max_itr, outter_max_itr should be larger than zero strictly. " 
     ϵ = epsilon; γ⁺ = gamma_upper; d̂ = d_hat
 
-    model_mp = Model(HiGHS.Optimizer); mp = MP(model_mp, γ⁺); set_silent(model_mp)
-    model_msp = Model(HiGHS.Optimizer); msp = MSP(model_msp, d̂, γ⁺); set_silent(model_msp)
+    model_mp = Model(() -> Gurobi.Optimizer(GUROBI_ENV)); mp = MP(model_mp, γ⁺); set_silent(model_mp)
+    model_msp = Model(() -> Gurobi.Optimizer(GUROBI_ENV)); msp = MSP(model_msp, d̂, γ⁺); set_silent(model_msp)
 
     PortOutVariable!(mp, :d) do d fix.(d, d̂, force=true) end
     PortOutVariable!(mp, :v) do v fix.(v, 0, force=true) end
@@ -162,14 +164,16 @@ function CCGAOutterLoop(
     OuterCounter = 0
     OuterResults = CCGAResults()
     for II in 1:outter_max_itr
+        println("Outter Forloop itr=$II")
         OuterCounter += 1
-        Results = CCGAInnerLoop(GetGamma(msp), w̄, d̂, ϵ, sparse_vee=false)
+        Results = CCGAInnerLoop(GetGamma(msp), w̄, d̂, epsilon=ϵ, sparse_vee=false)
         OuterResults(Results)
         if make_plot
             fig = plot(Results.upper_bounds, label="upper_fmp", marker=:x)
             plot!(fig, Results.lower_bounds, label="lower_fsp", marker=:x)
             fig|>display
         end
+
         if Results.upper_bounds[end] < 1e-4
             @info "Outter for loop terminated due to convergence of FMP FSP to an objective value of zero. "
             break
@@ -180,17 +184,17 @@ function CCGAOutterLoop(
             GetRhoPlus(Results.fmp), 
             GetRhoMinus(Results.fmp)
         )
-
+        @info "Introduced cut to the msp and we are solving it. "
         Solve!(msp)
         @info "Objective value of msp settled at: $(objective_value(msp)). "
         w̄ = Getw(msp)
     end
-
+    OuterResults.msp = msp
 return OuterResults end
 
 
 
-### =====================================================================================================================
+### ====================================================================================================================
 ### Problems we are identifying: 
 ### Trying to executing the CCGA Inner forloops and see what could be causing the infeasibility to the cut to the master 
 ### problem. 
@@ -198,42 +202,6 @@ return OuterResults end
 ϵ = 0.1
 γ_upper = 100
 d̂ = 200*(size(MatrixConstruct.H, 2)|>ones)
-CCGAOutterLoop(d̂, γ_upper)
-
-
-# model_mp = Model(HiGHS.Optimizer); mp = MP(model_mp, γ̄); set_silent(model_mp)
-# model_msp = Model(HiGHS.Optimizer); msp = MSP(model_msp, d̂, γ̄); set_silent(model_msp)
-# PortOutVariable!(mp, :d) do d fix.(d, d̂, force=true) end
-# PortOutVariable!(mp, :v) do v fix.(v, 0, force=true) end
-# Solve!(mp)
-# w̄ = Getw(mp)
-# Solve!(msp)
-
-# OuterCounter = 0
-# OuterResults = CCGAResults()
-# for II in 1:10
-#     OuterCounter += 1
-#     Results = CCGAInnerLoop(GetGamma(msp), w̄, d̂, sparse_vee=false)
-#     OuterResults(Results)
-#     fig = plot(Results.upper_bounds, label="upper_fmp", marker=:x)
-#     plot!(fig, Results.lower_bounds, label="lower_fsp", marker=:x)
-#     fig|>display
-#     if Results.upper_bounds[end] < 1e-4
-#         @info "Outter for loop terminated due to convergence of FMP FSP to an objective value of zero. "
-#         break
-#     end
-
-
-#     IntroduceCut!(
-#         msp, 
-#         GetRhoPlus(Results.fmp), 
-#         GetRhoMinus(Results.fmp)
-#     )
-
-#     Solve!(msp)
-#     @info "Objective value of msp settled at: $(objective_value(msp)). "
-#     w̄ = Getw(msp)
-    
-# end
+Results = CCGAOutterLoop(d̂, γ_upper);
 
 
