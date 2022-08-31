1326cab36d3864542e8c0cad6165e41997c1da32
AL <32211153+iluvjava@users.noreply.github.com>
2022-08-30 21:15:14 -0700

    >w<

--- src/problem_parameters.jl
+++ src/problem_parameters.jl
@@ -22,7 +22,7 @@ mutable struct ConstParameters
 
     function ConstParameters()
         this = new()
-        this.HORIZON = 12; 
+        this.HORIZON = 4; 
         this.Φ = 1e8; 
         this.RREGD = zeros(this.HORIZON)
         this.RREGU = zeros(this.HORIZON)
