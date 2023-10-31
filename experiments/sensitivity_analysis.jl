3fd4f631c4ee1f73de1e30a1d00c63ef0e27e899
alto <victor13011660040@gmail.com>
2023-10-30 17:11:51 -0700

    >w<

--- experiments/sensitivity_analysis.jl
+++ experiments/sensitivity_analysis.jl
@@ -2,7 +2,7 @@ include("experiment_context_and_assembling.jl")
 
 # Experiment code here, for example: 
 global DEMANDS_PROFILES = "$(MatrixConstruct.DATA_DIR)/demand_profiles.csv"|>open|>CSV.File
-global PROFILE = 15
+global PROFILE = 13
 d̂ = [DEMANDS_PROFILES[PROFILE][idx] for idx in 2:(MatrixConstruct.CONST_PROBLEM_PARAMETERS.HORIZON + 1)]
 TOL = 1.0
 GAMMA_UPPER = 1000
@@ -19,4 +19,4 @@ Results = OuterLoop(
     make_plot=false, 
     msp_optimality_gap=0.05,
     session_time_out=7200
-);
+);  
