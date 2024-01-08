e3a7b3ffbda65cb41cb3c74f401c9a4391c1c134
alto <victor13011660040@gmail.com>
2024-01-08 14:09:18 -0800

    >w<

--- experiments/sensitivity_analysis.jl
+++ experiments/sensitivity_analysis.jl
@@ -17,7 +17,7 @@ Results = OuterLoop(
     inner_routine=InnerLoopHeuristic,
     block_demands=0, 
     make_plot=false, 
-    msp_optimality_gap=0.05,
+    msp_optimality_gap=0.001,
     session_time_out=7200
 );
 
