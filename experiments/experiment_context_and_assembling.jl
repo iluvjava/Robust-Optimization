017af92b5e2e4e7531c5acdf81f4bcdd3d20853f
alto <victor13011660040@gmail.com>
2024-01-18 17:23:44 -0800

    >w<

--- experiments/experiment_context_and_assembling.jl
+++ experiments/experiment_context_and_assembling.jl
@@ -7,7 +7,7 @@ include("../src/utilities.jl")
 
 module MatrixConstruct
     "Experiments data folder. "
-    DATA_DIR = "data_big"
+    DATA_DIR = "data"
 
     include("../src/coef_mgr_v2.jl")
     include("../src/problem_parameters.jl")
--- experiments/run_experiment.jl
+++ experiments/run_experiment.jl
@@ -2,10 +2,10 @@ include("experiment_context_and_assembling.jl")
 
 # Experiment code here, for example: 
 global DEMANDS_PROFILES = "$(MatrixConstruct.DATA_DIR)/demand_profiles.csv"|>open|>CSV.File
-global PROFILE = 19
+global PROFILE = 14
 d̂ = [DEMANDS_PROFILES[PROFILE][idx] for idx in 2:(MatrixConstruct.CONST_PROBLEM_PARAMETERS.HORIZON + 1)]
 TOL = 1.0
-GAMMA_UPPER = 1400
+GAMMA_UPPER = 2000
 
 Results = OuterLoop(
     d̂,
