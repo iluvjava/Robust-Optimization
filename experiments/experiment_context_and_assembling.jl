1e98b63bfdce19dd58c32a2865ce5c8f91f2142f
alto <victor13011660040@gmail.com>
2024-07-18 16:50:00 -0700

    >w< updated experiments

--- experiments/experiment_context_and_assembling.jl
+++ experiments/experiment_context_and_assembling.jl
@@ -14,9 +14,9 @@ module MatrixConstruct
     
     # Apply multiplers on different parameters for the data. The below is the default profile. 
     CONST_PROBLEM_PARAMETERS.HORIZON = 24
-    CONST_PROBLEM_PARAMETERS.Φ = 2400*1.75
+    CONST_PROBLEM_PARAMETERS.Φ = 2400
     for k in keys(DEMAND_RESPONSE.R)
-        DEMAND_RESPONSE.R[k] *= 1
+        DEMAND_RESPONSE.R[k] *= 3.5
     end
     STORAGE_SYSTEM.Capacity[1] = 100
     PRIMARY_GENERATORS.Pmax .*= 1
